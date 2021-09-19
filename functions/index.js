ed0b4174b38b2f3e761a14a76d8ab2c864a07440
protect-me <pmfwiww@gmail.com>
2021-09-19 22:27:13 +0900

    Feat: firease functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var admin = require('firebase-admin')
+
+var serviceAccount = require('./vue-tennis-key.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://vue-tennis-default-rtdb.firebaseio.com',
+})
+
+const db = admin.database()
+const fdb = admin.firestore()
+
+exports.createUser = functions.auth.user().onCreate(async (user) => {
+  const { uid, email, displayName, photoURL } = user
+  const time = new Date()
+  const userInfo = {
+    email,
+    displayName,
+    photoURL,
+    likeRequestList: [],
+    createdAt: time,
+    level: email == functions.config().admin.email ? 0 : 5,
+  }
+  await fdb.collection('users').doc(uid).set(userInfo) // set user at Firestore
+  userInfo.createdAt = time.getTime()
+  db.ref('users').child(uid).set(userInfo) // set user at RealTime Database
+})
+
+exports.deleteUser = functions.auth.user().onDelete(async (user) => {
+  const { uid } = user
+  await db.ref('users').child(uid).remove()
+  await fdb.collection('users').doc(uid).delete()
+})
