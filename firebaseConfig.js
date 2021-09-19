86facaadc8348e22450d0742e635676ba3fd806f
protect-me <pmfwiww@gmail.com>
2021-09-19 13:39:22 +0900

    Feat: set dotenv

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 export default {
-  apiKey: 'AIzaSyDxgam8LdFUmbJbdwEcAIJUeFl5pM5i_Ms',
-  authDomain: 'vue-tennis.firebaseapp.com',
-  databaseURL: 'https://vue-tennis-default-rtdb.firebaseio.com',
-  projectId: 'vue-tennis',
-  storageBucket: 'vue-tennis.appspot.com',
-  messagingSenderId: '473088626179',
-  appId: '1:473088626179:web:9d73a960a68e9abce74836',
-  measurementId: 'G-D35392B2ZY',
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId,
+  measurementId: process.env.VUE_APP_measurementId,
 }
