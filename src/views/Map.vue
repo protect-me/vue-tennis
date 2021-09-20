bd32d673a83dcf2dd69caf8b3ab45641c5f0990d
protect-me <pmfwiww@gmail.com>
2021-09-20 12:44:42 +0900

    Feat: set nav

---
+++ src/views/Map.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="map-container">
+    <div>map</div>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.map-container {
+  width: 100%;
+  height: 100%;
+  border: 1px solid green;
+}
+</style>
