f7bddd5f5db360ccaa049b524f47b8e63295b0f6
protect-me <pmfwiww@gmail.com>
2021-09-28 13:07:27 +0900

    Feat: FindPeopleRegist style

--- src/views/FindPeople/FindPeopleRegist.vue
+++ src/views/FindPeople/FindPeopleRegist.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="find-people-regist-container">
-    <v-card flat>
+    <v-card flat class="find-people-regist-content">
       <div class="find-people-regist-header">
         <TitleWithButton
           titleText="신규 게스트 모집 등록"
@@ -33,40 +33,40 @@
           ></v-select>
         </div>
 
-        <v-menu
-          v-model="dateMenu"
-          :close-on-content-click="false"
-          offset-y
-          transition="scale-transition"
-          min-width="auto"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              bottom
-              left
-              class="mb-3"
-              outlined
-              v-model="form.date"
-              label="일시"
-              readonly
-              hide-details
-              v-bind="attrs"
-              v-on="on"
-              :rules="[rules.required, rules.beforeToday]"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            locale="ko-KR"
-            v-model="form.date"
-            @input="dateMenu = false"
-            full-width
-          ></v-date-picker>
-        </v-menu>
-
         <div class="divide-column">
           <v-menu
+            v-model="dateMenu"
+            :close-on-content-click="false"
+            offset-y
+            transition="scale-transition"
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                bottom
+                left
+                class="mb-3 mr-3"
+                outlined
+                v-model="form.date"
+                label="일시"
+                readonly
+                hide-details
+                v-bind="attrs"
+                v-on="on"
+                :rules="[rules.required, rules.beforeToday]"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              locale="ko-KR"
+              v-model="form.date"
+              @input="dateMenu = false"
+              full-width
+            ></v-date-picker>
+          </v-menu>
+
+          <v-menu
             bottom
-            right
+            :nudge-left="startTimeNudgeLeft"
             v-model="startTimeMenu"
             :close-on-content-click="false"
             offset-y
@@ -154,15 +154,14 @@
 
         <div class="divide-column">
           <v-text-field
-            class="mb-3 mr-2"
+            class="mb-3 mr-3"
             label="구하는 인원(명)"
             v-model="form.vacant"
             type="number"
             outlined
             hide-details
-            :rules="[rules.required]"
+            :rules="[rules.required, rules.vacantCount]"
           />
-          <div class="title mr-2 mt-3">/</div>
           <v-text-field
             class="mb-3"
             label="총 인원(명)"
@@ -170,13 +169,13 @@
             type="number"
             outlined
             hide-details
-            :rules="[rules.required]"
+            :rules="[rules.required, rules.totalCount]"
           />
         </div>
 
         <div class="divide-column">
           <v-text-field
-            class="mb-3 mr-6"
+            class="mb-3 mr-3"
             label="연락처 | 메신저"
             v-model="form.contact"
             type="text"
@@ -208,7 +207,14 @@
       </v-form>
     </v-card>
     <v-spacer></v-spacer>
-    <v-btn class="compelete-btn" block color="primary" @click="apply">
+    <v-btn
+      class="compelete-btn"
+      block
+      color="primary"
+      @click="apply"
+      :disabled="isProcessing"
+      :loading="isProcessing"
+    >
       신규 등록
     </v-btn>
 
@@ -242,6 +248,10 @@ export default {
   },
   computed: {
     ...mapState(['fireUser', 'user']),
+    startTimeNudgeLeft() {
+      let vMenuWidth = (window.innerWidth - 36) / 3
+      return `${(290 - vMenuWidth) / 2}px`
+    },
   },
   data() {
     return {
@@ -251,6 +261,11 @@ export default {
       rules: {
         required: (value) => !!value || '필수 기입',
         counter: (value) => value.length <= 100 || '100자 이하로 입력해주세요',
+        vacantCount: (value) =>
+          (value <= 10 && value >= 0) || '0~10 사이의 숫자를 입력해주세요',
+        totalCount: (value) =>
+          (value <= 10 && value >= 0 && value >= Number(this.form.vacant)) ||
+          '0~10 사이의 숫자를 입력해주세요',
         beforeToday: (value) => {
           const inputDate = new Date(value)
           const today = new Date().setHours(0)
@@ -386,6 +401,11 @@ export default {
   height: calc(100vh - 48px);
   display: flex;
   flex-direction: column;
+
+  .find-people-regist-content {
+    height: calc(100vh - 120px);
+    overflow: scroll;
+  }
   .compelete-btn {
     max-height: 36px;
   }
